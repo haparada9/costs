836cf5f8dad1374ae4d3f6bfe3eeed2e9968d9fc
Hugo de Assis Parada <ha.parada9@gmail.com>
2022-05-16 18:58:54 -0300

    feat: :sparkles: Insere dados no banco via API

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -1,13 +1,19 @@
 import styles from './Select.module.css'
 
-function Select({text, name, options, handleOnChange, value}) {
+function Select({ text, name, options, handleOnChange, value }) {
     return (
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}:</label>
-            <select name={name} id={name}>
+            <select 
+            name={name} 
+            id={name} 
+            onChange={handleOnChange} 
+            value={value || ''}
+            >
                 <option>Selecione uma opção</option>
                 {options.map((option) => (
-                    <option value={option.id} key={option.id}>{option.name}
+                    <option value={option.id} key={option.id}>
+                        {option.name}
                     </option>
                 ))}
             </select>   

95fcd58ae15ff0e9ffeccd72ec12b73985278d25
Hugo de Assis Parada <ha.parada9@gmail.com>
2022-05-18 10:58:20 -0300

    feat: :sparkles: Implementação de mensagem do sistema após criação de projeto

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,20 @@
-function Project() {
-    return <h1>Projetos</h1>
-}
+import { useLocation } from 'react-router-dom'
 
+import Message from '../layout/Message'
+
+function Project() {
+
+    const location = useLocation()
+    let message = ''
+    if(location.state) {
+        message = location.state.message
+    }
+
+    return (
+        <div>
+            <h1>Meus Projetos</h1>
+            {message && <Message type="success" msg={message} />}
+        </div>
+    )
+}
 export default Project

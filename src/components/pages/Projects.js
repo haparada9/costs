a7952d975d5b8f46f91a59a06647d0073561b94d
Hugo de Assis Parada <ha.parada9@gmail.com>
2022-05-18 11:21:32 -0300

    feat: :sparkles: Cria o dashboard

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,10 @@
 import { useLocation } from 'react-router-dom'
 
 import Message from '../layout/Message'
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
+
+import styles from './Projects.module.css'
 
 function Project() {
 
@@ -11,10 +15,16 @@ function Project() {
     }
 
     return (
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto" />
+            </div>
             {message && <Message type="success" msg={message} />}
-        </div>
+            <Container customClass="start">
+                <p>Projetos</p>
+            </Container>
+        </div> 
     )
 }
 export default Project
